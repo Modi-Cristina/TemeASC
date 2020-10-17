cdb807af310cebf62ed10a79bef37dca011c5297
Modi-Cristina <modi.cristina@gmail.com>
2020-10-17 22:16:27 +0300

    tema1

---
+++ tema1/tema1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tema1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int ptpc;
+            Console.Write("Introduceti puterea de calcul dorita:");
+            ptpc=Int32.Parse(Console.ReadLine());
+          
+           float timp=new Single();
+            timp= 1.5f * (float)Math.Log(ptpc,2);
+
+
+            Console.WriteLine($"Puterea calculatorului va fi de {ptpc} ori mai mare peste {timp} ani .");
+
+        }
+    }
+}
